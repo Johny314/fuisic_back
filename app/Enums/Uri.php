5a8bb866c2a9824b811457d11c0721410a24de46
Johny314 <Jekazombi@gmail.com>
2025-03-03 17:41:55 +0500

    005 | URI fix

--- app/Enums/Uri.php
+++ app/Enums/Uri.php
@@ -5,20 +5,20 @@ namespace App\Enums;
 enum Uri: string
 {
     case card_set = 'card_set';
-    case card_set_id = 'card_set_id';
+    case card_set_id = 'card_set/{card_set}';
 
     case card = 'card';
-    case card_id = 'card_id';
+    case card_id = 'card/{card}';
 
     case test = 'test';
-    case test_id = 'test_id';
+    case test_id = 'test/{test}';
 
     case task = 'task';
-    case task_id = 'task_id';
+    case task_id = 'task/{task}';
 
     case section = 'section';
-    case section_id = 'section_id';
+    case section_id = 'section/{section}';
 
     case user = 'user';
-    case user_id = 'user_id';
+    case user_id = 'user/{user}';
 }
